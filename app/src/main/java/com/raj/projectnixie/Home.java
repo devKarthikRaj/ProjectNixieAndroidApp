0f695bc1c3894d4b389ad3b152b3c41d375aa729
devKarthikRaj <rocketraj2000@gmail.com>
2021-01-24 17:52:20 +0800

    Splash Screen and Home Activity Code Partially Completed

--- app/src/main/java/com/raj/projectnixie/Home.java
+++ app/src/main/java/com/raj/projectnixie/Home.java
@@ -1,15 +1,49 @@
 package com.raj.projectnixie;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
 
-public class Home extends AppCompatActivity {
+public class Home extends Activity implements OnClickListener {
     private static final String TAG = "Home";
+
+    ImageButton timeModeBtn;
+    ImageButton dateModeBtn;
+    ImageButton CountdownModeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        timeModeBtn = findViewById(R.id.Time_Mode_Button);
+        dateModeBtn = findViewById(R.id.Date_Mode_Button);
+        CountdownModeBtn = findViewById(R.id.Countdown_Mode_Button);
+
+        timeModeBtn.setOnClickListener(this);
+        dateModeBtn.setOnClickListener(this);
+        CountdownModeBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.Time_Mode_Button:
+                Log.d(TAG, "Time Mode Button Clicked");
+                startActivity(new Intent(Home.this, TimeMode.class));
+                break;
+            case R.id.Date_Mode_Button:
+                Log.d(TAG, "Date Mode Button Clicked");
+                startActivity(new Intent(Home.this, DateMode.class));
+                break;
+            case R.id.Countdown_Mode_Button:
+                Log.d(TAG, "Countdown Mode Button Clicked");
+                startActivity(new Intent(Home.this, CountdownMode.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/raj/projectnixie/SplashScreen.java
+++ app/src/main/java/com/raj/projectnixie/SplashScreen.java
@@ -2,14 +2,42 @@ package com.raj.projectnixie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
     private static final String TAG = "SplashScreen";
+
+    private static int SPLASH_TIME_OUT=5000;
+    private boolean connectionStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //Establishing connection with Nixie Hardware
+        Log.d(TAG,"Establishing connection with ESP32");
+        //tbc
+        connectionStatus = true; //for testing purposes
+        if(connectionStatus == true) {
+            //---Splash Screen---
+            Log.d(TAG,"Connection successfully established with ESP32");
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashScreen.this, Home.class));
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
+        else {
+            Log.d(TAG,"Unable to establish connection with ESP32");
+            Toast.makeText(this,"Unable to establish connection with ESP32",Toast.LENGTH_LONG);
+        }
     }
 }
